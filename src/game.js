1743a05ca4d7b207847abde93484e08bd54aaf43
vicxi <victorgp1124@mail.com>
2022-10-04 18:30:42 -0500

    fixed

--- src/game.js
+++ src/game.js
@@ -34,9 +34,9 @@ window.addEventListener("resize", setCanvasSize);
 
 function setCanvasSize() {
   if (window.innerHeight > window.innerWidth) {
-    canvasSize = window.innerWidth * 0.7;
+    canvasSize = window.innerWidth * 0.8;
   } else {
-    canvasSize = window.innerHeight * 0.7;
+    canvasSize = window.innerHeight * 0.8;
   }
 
   canvasSize = Number(canvasSize.toFixed(0));
